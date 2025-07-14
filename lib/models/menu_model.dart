5c18ab3e35bf5195688841c12ff4a3395cf93c9d
Igbana <igbanaaondosoo13@gmail.com>
2025-07-14 09:51:25 +0100

    added side menu, created menu models

---
+++ lib/models/menu_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MenuModel {
+  const MenuModel({required this.icon, required this.title});
+
+  final IconData icon;
+  final String title;
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/widgets/side_menu.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(flex: 2, child: SizedBox(child: SideMenu())),
+            Expanded(flex: 7, child: Container(color: Colors.blue)),
+            Expanded(flex: 3, child: Container(color: Colors.green)),
+          ],
+        ),
+      ),
+    );
   }
 }
