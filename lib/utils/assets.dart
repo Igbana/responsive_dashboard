8a8bf088fab3852079badad30247c12fee4ad890
Igbana <igbanaaondosoo13@gmail.com>
2025-07-14 09:33:43 +0100

    Asset management

---
+++ lib/utils/assets.dart
@@ -0,0 +1,11 @@
+class AppAssets {
+  static final String burn = "assets/icons/burn.jpg";
+  static final String distance = "assets/icons/distance.jpg";
+  static final String sleep = "assets/icons/sleep.jpg";
+  static final String steps = "assets/icons/steps.jpg";
+  static final String unknown = "assets/icons/unknown.svg";
+
+  static final String media = "assets/icons/media.svg";
+  static final String foler = "assets/icons/folder.svg";
+  static final String documents = "assets/icons/Documents.svg";
+}
