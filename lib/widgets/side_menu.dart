a7e27821b4ec1c7a3b157d00cbce211698387cc7
Igbana <igbanaaondosoo13@gmail.com>
2025-07-14 10:08:36 +0100

    Done with side menu

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/data/side_menu_data.dart';
+import 'package:responsive_dashboard/utils/colors.dart';
 
 class SideMenu extends StatefulWidget {
   const SideMenu({super.key});
@@ -8,8 +10,53 @@ class SideMenu extends StatefulWidget {
 }
 
 class _SideMenuState extends State<SideMenu> {
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 80, horizontal: 20),
+      child: ListView.builder(
+        itemCount: SideMenuData.menu.length,
+        itemBuilder: (context, index) {
+          final bool isSelected = selectedIndex == index;
+          return Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: isSelected ? AppColors.selectionColor : Colors.transparent,
+            ),
+            child: InkWell(
+              onTap: () => setState(() {
+                selectedIndex = index;
+              }),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 13.0,
+                      vertical: 7,
+                    ),
+                    child: Icon(
+                      SideMenuData.menu[index].icon,
+                      color: isSelected ? Colors.black : Colors.grey,
+                    ),
+                  ),
+                  Text(
+                    SideMenuData.menu[index].title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: isSelected ? Colors.black : Colors.grey,
+                      fontWeight: isSelected
+                          ? FontWeight.w600
+                          : FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
